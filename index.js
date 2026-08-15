7c5e9599d7f7c078d0d351d20c1476e8a9dccfd1
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-08-15 16:25:52 +0500

    Added the generator

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
 import inquirer from 'inquirer';
 import fs from 'fs';
+import qr from 'qr-image';
+
 inquirer
     .prompt([
         {
@@ -14,13 +11,19 @@ inquirer
         }
     ])
     .then((answers) => {
+        // 1. Save the URL to a text file
         fs.writeFileSync('url.txt', answers.url);
-        // Use user feedback for... whatever!!
+        console.log('Saved URL to url.txt!');
+
+        // 2. Generate the QR code using the user's input directly
+        const qr_svg = qr.image(answers.url, { type: 'svg' });
+        qr_svg.pipe(fs.createWriteStream('i_love_qr.svg'));
+        console.log('QR Code generated successfully as i_love_qr.svg!');
     })
     .catch((error) => {
         if (error.isTtyError) {
             console.error('Prompt couldn\'t be rendered in the current environment');
         } else {
-            console.error('Something else went wrong');
+            console.error('Something else went wrong:', error);
         }
     });
